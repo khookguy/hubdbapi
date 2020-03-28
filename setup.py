9d6509f9feb56f95f5fdb6f371ca2f7ebbf2ac41
Mark Hansen <mark@Marks-MBP.fios-router.home>
2020-03-28 12:23:52 -0400

    update setup to v0.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'hubdbapi',
   packages = ['hubdbapi'],
-  version = '0.2',
+  version = '0.3',
   license='agpl-3.0',
   description = 'Python utilities for integrating with the HubDB API.',
   author = 'Mark Hansen',
   author_email = 'mark@rarekarma.com',
   url = 'https://github.com/khookguy/hubdbapi',
-  download_url = 'https://github.com/khookguy/hubdbapi/archive/v0.2.tar.gz',
+  download_url = 'https://github.com/khookguy/hubdbapi/archive/v0.3.tar.gz',
   keywords = ['HubSpot', 'HubDB', 'API'],
   install_requires=[
       ],
